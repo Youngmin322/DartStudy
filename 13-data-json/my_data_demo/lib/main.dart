e42b3f56fe2cfc05a146710948a23dec32d5d98f
Youngmin Cho <choyoungmin0322@gmail.com>
2025-02-17 14:13:22 +0900

    JSON 파일 삭제 및 텍스트 파일로 데이터 로드 방식 변경

--- 13-data-json/my_data_demo/lib/main.dart
+++ 13-data-json/my_data_demo/lib/main.dart
@@ -1,61 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
+import 'package:my_data_demo/_2_with_file.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
- const MyApp({super.key});
-
- @override
- State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late Future<List<String>> futureData;
-
-  @override 
-  void initState() {
-    super.initState();
-    futureData = _loadMonths();
-  }
-
-  Future<List<String>> _loadMonths() async {
-    String jsonString = await DefaultAssetBundle.of(
-      context,
-      ).loadString('assets/example.json');
-      // json 파싱된 결과물 List<String>으로 변환
-      return jsonDecode(jsonString).values.cast<String>().toList();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('My Data App')),
-        body: FutureBuilder<List<String>>(
-          future: futureData, 
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              // 미래 데이터에 대해 값이 없는 경우 (1. 값이 안 넘어온 경우, 2. 로딩 중, 3. 에러)
-              return const Center(child: CircularProgressIndicator());
-            }
-          final data = snapshot.data!;
-          return ListView.builder(
-          itemCount: data.length,
-          itemBuilder: (context, index) {
-            return ListTile(title: Text(data[index]));
-              },
-            );
-          },
-        ),
-      ),
-    );
-  }
 }
